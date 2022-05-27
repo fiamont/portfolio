a7cb93272e147439b6a49b24b582702709a8fb3a
Sofia <sofia.montgomery@gmail.com>
2022-05-27 03:09:32 +0200

    edit about css

--- components/About.jsx
+++ components/About.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <div className={style.AboutMe} id="about">
-      <div className={style.Container}>
-        <h2>
-          Jag heter Sofia Montgomery och jag studerar till javautvecklare på
-          Stockholms Tekniska Institut.
-        </h2>
-        <div className={style.Text}>
-          <p id="text1">
+    <div className={style.AboutMe}>
+      <h2 className={style.heading}>
+        Jag heter Sofia Montgomery och jag studerar till javautvecklare på
+        Stockholms Tekniska Institut.
+      </h2>
+      <div className={style.part1}>
+        <div className={style.textpart2}>
+          <p>
             Nyfikenheten på programmering har funnits länge, men jag har
             tidigare fokuserat på mitt stora intresse för djur och natur. Men
             eftersom jag bl.a. är allergisk mot både djur och växter har det
@@ -21,6 +21,13 @@ const About = () => {
             och på förskola. Efter en period som dataregistrerare insåg jag att
             jag trivs med arbetsmiljön och möjligheten att jobba både i team och
             självständigt passar mig, men att jag vill ha större utmaningar.
+          </p>
+          <p>
+            Nu har jag valt att inrikta mig på programmering, vilket känns
+            väldigt spännande. Jag skulle beskriva mig själv som en glad, lugn,
+            noggrann och praktisk person; jag är en lyssnare som pratar när jag
+            har något att säga. Jag är kreativ och gillar att strukturera upp
+            saker och hålla ordning, och att lösa problem.
           </p>
         </div>
         <div className={style.Photo}>
@@ -32,20 +39,11 @@ const About = () => {
             height={228}
           />
         </div>
-        <div className={style.Text2}>
-          <p>
-            Nu har jag valt att inrikta mig på programmering, vilket känns
-            väldigt spännande. Jag skulle beskriva mig själv som en glad, lugn,
-            noggrann och praktisk person; jag är en lyssnare som pratar när jag
-            har något att säga. Jag är kreativ och gillar att strukturera upp
-            saker och hålla ordning, och att lösa problem.
-          </p>
-          <h2>
-            Drömjobbet vore att kunna kombinera mina programmeringskunskaper med
-            mitt engagemang för djur, natur, miljö och klimat.
-          </h2>
-        </div>
       </div>
+      <h2 className={style.heading2}>
+        Drömjobbet vore att kunna kombinera mina programmeringskunskaper med
+        mitt engagemang för djur, natur, miljö och klimat.
+      </h2>
     </div>
   );
 };
--- components/Intro.jsx
+++ components/Intro.jsx
@@ -2,35 +2,25 @@ import style from "../styles/Intro.module.css";
 import Girlcartoon from "../public/girlcartoon.png";
 import Image from "next/image";
 import Link from "next/link";
-// import styled, { keyframes } from "styled-components";
-// import { bounceInRight } from "react-animations";
 
 const Intro = () => {
-  //   const bouncyAnimation = keyframes`${bounceInRight}`;
-
-  //   const Bouncy = styled.div`
-  //     animation: 5s ${bouncyAnimation};
-  //   `;
   return (
-    <div className={style.Intro} id="intro">
-      <div className={style.Container}>
-        <h2>
-          Jag heter Sofia Montgomery och jag studerar till javautvecklare på
-          Stockholms Tekniska Institut.
-        </h2>
-        <div className={style.Text}>
-          <p>
-            Hösten 2021 började jag mina studier på STI, hösten 2022 är det dags
-            för första praktikperioden och våren 2023 tar jag min examen. Läs
-            mer om mig och mitt äventyr
-            <Link href="/ommig" passHref>
-              <a className={style.Link}>här</a>
-            </Link>
-            .
-          </p>
-        </div>
+    <div className={style.Intro}>
+      <h2 className={style.Rubrik}>
+        Jag heter Sofia Montgomery och jag studerar till javautvecklare på
+        Stockholms Tekniska Institut.
+      </h2>
+      <div className={style.TextImg}>
+        <p className={style.P}>
+          Hösten 2021 började jag mina studier på STI, hösten 2022 är det dags
+          för första praktikperioden och våren 2023 tar jag min examen. Läs mer
+          om mig och mitt äventyr
+          <Link href="/ommig" passHref>
+            <a className={style.Link}>här</a>
+          </Link>
+          .
+        </p>
         <div className={style.Photo}>
-          {/* <Bouncy> */}
           <Image
             className={style.Girlcartoon}
             src={Girlcartoon}
@@ -38,7 +28,6 @@ const Intro = () => {
             width={223}
             height={200}
           />
-          {/* </Bouncy> */}
         </div>
       </div>
     </div>
