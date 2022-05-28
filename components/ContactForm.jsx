1e61ea491305f3c60c5223385c8926375312cf06
Sofia <sofia.montgomery@gmail.com>
2022-05-28 16:27:36 +0200

    edit linkedIn-link

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -3,16 +3,21 @@ import style from "../styles/ContactForm.module.css";
 import Link from "next/link";
 
 function ContactForm() {
+  const handleURL = (url) => {
+    return () => window.open(url, "_blank");
+  };
   return (
     <div className={style.contact}>
       <h2 className={style.rubrik}>
         Vill du komma i kontakt med mig? Kontakta mig på{" "}
-        <Link
-          href="https://www.linkedin.com/in/sofia-montgomery-3175a821a"
-          passHref
+        <a
+          className={style.Link}
+          onClick={handleURL(
+            "https://www.linkedin.com/in/sofia-montgomery-3175a821a"
+          )}
         >
-          <a className={style.Link}>LinkedIn</a>
-        </Link>
+          LinkedIn
+        </a>{" "}
         eller via kontaktformuläret nedan:
       </h2>
       <form className={style.form}>
@@ -22,17 +27,24 @@ function ContactForm() {
         <input
           className={style.input}
           type="text"
+          name="namn"
           placeholder="Namn Namnsson"
         />
         <label className={style.label} htmlFor="Email">
           Email
         </label>
-        <input className={style.input} type="text" placeholder="mail@hej.se" />
+        <input
+          className={style.input}
+          type="text"
+          placeholder="mail@hej.se"
+          name="email"
+        />
         <label className={style.label} htmlFor="Meddelande">
           Meddelande
         </label>
         <textarea
           className={style.textarea}
+          name="meddelande"
           placeholder="Skriv ditt meddelande här..."
         />
         <button className={style.button}>Skicka</button>
